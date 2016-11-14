98938891f1c4200273aa4a56553d78fc4f99e203
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-11-14 14:48:17 +0100

    Migrate the app to Spring Functional Web Framework
    This commit also removes the need for Gulp, and
    the application can be run with a simple gradle run
    command.

--- src/main/java/io/projectreactor/Application.java
+++ src/main/java/io/projectreactor/Application.java
@@ -1,63 +1,75 @@
 package io.projectreactor;
 
-import io.projectreactor.ratpack.EnableRatpack;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ratpack.error.ClientErrorHandler;
-import ratpack.func.Action;
-import ratpack.handling.Chain;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.web.reactive.function.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.RouterFunctions.route;
+import static org.springframework.web.reactive.function.ServerResponse.*;
+import reactor.ipc.netty.http.HttpServer;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.BodyInserters;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.accept.PathExtensionContentTypeResolver;
+import org.springframework.web.reactive.function.RouterFunction;
+import org.springframework.web.reactive.function.RouterFunctions;
 
 /**
  * Main Application for the Project Reactor home site.
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableRatpack
 public class Application {
 
-	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
-
-	@Bean
-	public ClientErrorHandler clientErrorHandler() {
-		return (ctx, statusCode) -> {
-			if (statusCode == 404) {
-				ctx.redirect("/404.html");
-			}
-			else {
-				LOG.error("client error: {}", statusCode);
-			}
-		};
+	public static void main(String... args) throws InterruptedException {
+		HttpHandler httpHandler = RouterFunctions.toHttpHandler(routes());
+		ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
+		HttpServer server = HttpServer.create("localhost", 8080);
+		server.startAndAwait(adapter);
 	}
 
-	@Bean
-	public Action<Chain> ratpack() {
-		return chain -> {
-			chain.prefix("docs/api",
-					c -> c.handler(h -> h.redirect(h.getRequest()
-					                                .getUri()
-					                                .replace("/docs/", "/old/"))))
-			     .prefix("docs/reference",
-					     c -> c.handler(h -> h.redirect(h.getRequest()
-					                                     .getUri()
-					                                     .replace("/docs/", "/old/"))))
-			     .prefix("core/docs/reference",
-					     c -> c.handler(h -> h.redirect("https://github.com/reactor/reactor-core/blob/master/README.md")))
-			     .prefix("docs/raw",
-					     c -> c.handler(h -> h.redirect(h.getRequest()
-					                                     .getUri()
-					                                     .replace("/docs/", "/old/"))));
-		};
-	}
+	private static RouterFunction<?> routes() {
 
-	public static void main(String... args) {
-		SpringApplication.run(Application.class, args);
+		Map<String, MediaType> mediaTypes = new HashMap<>();
+		mediaTypes.put("css", MediaType.valueOf("text/css"));
+		mediaTypes.put("html", MediaType.valueOf("text/html"));
+		mediaTypes.put("js", MediaType.valueOf("application/javascript"));
+		mediaTypes.put("png", MediaType.valueOf("image/png"));
+		mediaTypes.put("jpg", MediaType.valueOf("image/jpg"));
+		mediaTypes.put("ico", MediaType.valueOf("image/x-icon"));
+		mediaTypes.put("woff2", MediaType.valueOf("font/woff2"));
+		mediaTypes.put("woff", MediaType.valueOf("application/font-woff"));
+		mediaTypes.put("ttf", MediaType.valueOf("font/truetype"));
+		PathExtensionContentTypeResolver contentTypeResolver = new PathExtensionContentTypeResolver(mediaTypes);
+		contentTypeResolver.setIgnoreUnknownExtensions(false);
+
+		// TODO Use andRoute() when https://jira.spring.io/browse/SPR-14904 will be fixed
+		return route(GET("/docs/api/**"), request ->
+				status(FOUND).header(LOCATION, request.path().replace("/docs/", "/old/")).build()
+			).and(route(GET("/docs/reference/**"), request ->
+				status(FOUND).header(LOCATION, request.path().replace("/docs/", "/old/")).build()
+			).and(route(GET("/docs/raw/**"), request ->
+				status(FOUND).header(LOCATION, request.path().replace("/docs/", "/old/")).build()
+			).and(route(GET("/core/docs/reference/**"), request ->
+				status(FOUND).header(LOCATION, "https://github.com/reactor/reactor-core/blob/master/README.md").build()
+			).and(route(GET("/"), request ->
+				ok().body(BodyInserters.fromResource(new ClassPathResource("static/index.html")))
+			).and(route(GET("/docs"), request ->
+				ok().body(BodyInserters.fromResource(new ClassPathResource("static/docs/index.html")))
+			).and(route(GET("/{file}"), request ->
+				ok().body(BodyInserters.fromResource(new ClassPathResource("static/" + request.pathVariable("file"))))
+			).and(route(GET("/assets/{dir}/{file}"), request -> {
+					// TODO Simplify when https://jira.spring.io/browse/SPR-14905 will be fixed
+					Resource resource = new ClassPathResource("static/assets/" + request.pathVariable("dir") + "/" + request.pathVariable("file"));
+					return ok().contentType(contentTypeResolver.resolveMediaTypeForResource(resource)).body(BodyInserters.fromResource(resource));
+				}
+			)
+		)))))));
 	}
 
 }
