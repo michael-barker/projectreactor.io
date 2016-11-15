ec553d914473f14d385767c061787028dd3c1c0e
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-11-15 11:23:13 +0100

    No need for PathExtensionContentTypeResolver

--- src/main/kotlin/projectreactor/Application.kt
+++ src/main/kotlin/projectreactor/Application.kt
@@ -1,8 +1,5 @@
 package projectreactor
 
-
-import java.util.HashMap
-
 import org.springframework.http.HttpHeaders.*
 import org.springframework.http.HttpStatus.*
 import org.springframework.web.reactive.function.RequestPredicates.GET
@@ -12,10 +9,8 @@ import reactor.ipc.netty.http.HttpServer
 
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
-import org.springframework.http.MediaType
 import org.springframework.http.codec.BodyInserters
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
-import org.springframework.web.reactive.accept.PathExtensionContentTypeResolver
 import org.springframework.web.reactive.function.HandlerFunction
 import org.springframework.web.reactive.function.RouterFunction
 import org.springframework.web.reactive.function.RouterFunctions
@@ -34,19 +29,6 @@ object Application {
 
     private fun routes(): RouterFunction<*> {
 
-        val mediaTypes = HashMap<String, MediaType>()
-        mediaTypes.put("css", MediaType.valueOf("text/css"))
-        mediaTypes.put("html", MediaType.valueOf("text/html"))
-        mediaTypes.put("js", MediaType.valueOf("application/javascript"))
-        mediaTypes.put("png", MediaType.valueOf("image/png"))
-        mediaTypes.put("jpg", MediaType.valueOf("image/jpg"))
-        mediaTypes.put("ico", MediaType.valueOf("image/x-icon"))
-        mediaTypes.put("woff2", MediaType.valueOf("font/woff2"))
-        mediaTypes.put("woff", MediaType.valueOf("application/font-woff"))
-        mediaTypes.put("ttf", MediaType.valueOf("font/truetype"))
-        val contentTypeResolver = PathExtensionContentTypeResolver(mediaTypes)
-        contentTypeResolver.setIgnoreUnknownExtensions(false)
-
         return route(GET("/docs/api/**"), HandlerFunction { request -> status(FOUND).header(LOCATION, request.path().replace("/docs/", "/old/")).build() })
                 .andRoute(GET("/docs/reference/**"), HandlerFunction { request -> status(FOUND).header(LOCATION, request.path().replace("/docs/", "/old/")).build() })
                 .andRoute(GET("/docs/raw/**"), HandlerFunction { request -> status(FOUND).header(LOCATION, request.path().replace("/docs/", "/old/")).build() })
@@ -55,9 +37,8 @@ object Application {
                 .andRoute(GET("/docs"), HandlerFunction { request -> ok().body(BodyInserters.fromResource(ClassPathResource("static/docs/index.html"))) })
                 .andRoute(GET("/{file}"), HandlerFunction { request -> ok().body(BodyInserters.fromResource(ClassPathResource("static/" + request.pathVariable("file")))) })
                 .andRoute(GET("/assets/{dir}/{file}"), HandlerFunction { request ->
-                    // TODO Simplify when https://jira.spring.io/browse/SPR-14905 will be fixed
                     val resource = ClassPathResource("static/assets/" + request.pathVariable("dir") + "/" + request.pathVariable("file"))
-                    ok().contentType(contentTypeResolver.resolveMediaTypeForResource(resource)).body(BodyInserters.fromResource<Resource>(resource))
+                    ok().body(BodyInserters.fromResource<Resource>(resource))
                 })
     }
 }
